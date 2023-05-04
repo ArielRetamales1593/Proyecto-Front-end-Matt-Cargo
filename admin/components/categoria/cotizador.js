263c86a30fa788243d28025105cfd6d6850e2dbe
Ariel Retamales <nretamales15@gmail.com>
2023-05-04 00:44:24 -0400

    cotizador y responsive al 80%

--- admin/components/categoria/cotizador.js
+++ admin/components/categoria/cotizador.js
@@ -8,6 +8,57 @@ const font = Poppins({
 import Image from "next/image";
 
 export default function CategoriaListado({ categorias, setCategorias }) {
+  const [origen, setOrigen] = useState("");
+  const [destino, setDestino] = useState("");
+  const [peso, setPeso] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [precioTotal, setPrecioTotal] = useState("");
+
+  const [pesoFinal, setPesoFinal] = useState("");
+
+  function calcularPrecio() {
+    console.log("Origen:", origen);
+    console.log("Destino:", destino);
+    console.log("Peso:", peso);
+
+    if (origen === "Antofagasta" && destino === "Iquique") {
+      setPrecio(1000);
+    } else if (origen === "Iquique" && destino === "Calama") {
+      setPrecio(1500);
+    } else if (origen === "Calama" && destino === "Arica") {
+      setPrecio(2500);
+    } else if (origen === "Iquique" && destino === "Santiago") {
+      setPrecio(25000);
+    } else {
+      setPrecio(0);
+    }
+
+    const x = totalPeso(peso);
+
+    console.log(pesoFinal);
+    // Calculamos el precio total según el peso y la distancia
+    setPrecioTotal(precio + pesoFinal);
+
+    // Mostramos el resultado en la etiqueta p con el id "resultado"
+    const resultadoEl = document.getElementById("resultado");
+    resultadoEl.textContent = `El precio total es: $${precioTotal}`;
+
+    // useEffect(() => {
+    //   const resultadoEl = document.getElementById("resultado");
+    //   resultadoEl.textContent = `El precio total es: $${precioTotal}`;
+    // }, [precioTotal]);
+  }
+  function totalPeso(kg) {
+    if (kg > 0 && kg < 99) {
+      setPesoFinal(50000);
+    } else if (kg >= 100 && kg <= 199) {
+      setPesoFinal(100000);
+    } else if (kg >= 200 && kg <= 299) {
+      setPesoFinal(200000);
+    }
+    return pesoFinal;
+  }
+
   const cargarDatos = async () => {
     try {
       const baseUrl = "http://localhost:3000";
@@ -56,8 +107,8 @@ export default function CategoriaListado({ categorias, setCategorias }) {
         <div className="contGranate"></div>
         <form id="formCotizador">
           <h1> Cotizador de envios</h1>
-          <label for="origen">Origen:</label>
-          <select>
+          <label htmlFor="origen">Origen:</label>
+          <select value={origen} onChange={(e) => setOrigen(e.target.value)}>
             <option value="0">Selecciona una cuidad de origen</option>
             {categorias.map((categoria) => (
               <option key={categoria.id}>
@@ -65,9 +116,8 @@ export default function CategoriaListado({ categorias, setCategorias }) {
               </option>
             ))}
           </select>
-
           <label htmlfor="origen">Destino:</label>
-          <select>
+          <select value={destino} onChange={(e) => setDestino(e.target.value)}>
             <option value="0">Selecciona una cuida de destino</option>
             {categorias.map((categoria) => (
               <option key={categoria.id}>
@@ -75,13 +125,17 @@ export default function CategoriaListado({ categorias, setCategorias }) {
               </option>
             ))}
           </select>
-          <label for="peso">Peso (en kg):</label>
-          <input type="number" id="peso" required />
-
-          <button type="button" onclick="calcularPrecio()">
+          <label htmlFor="peso">Peso (en kg):</label>
+          <input
+            type="number"
+            id="peso"
+            value={peso}
+            onChange={(e) => setPeso(e.target.value)}
+            required
+          />
+          <button type="button" onClick={calcularPrecio}>
             Calcular precio
           </button>
-
           <p id="resultado"></p>
         </form>
         <Image
